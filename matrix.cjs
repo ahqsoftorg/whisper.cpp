d1e4e15220211227f6767afbc7b20d838c43f621
Akshanabha Chakraborty <ahqsecret@gmail.com>
2025-11-24 22:42:37 +0530

    build

--- matrix.cjs
+++ matrix.cjs
@@ -2,19 +2,25 @@ const generateAllCombinations = require("./util.cjs");
 
 // { os: 'windows', runner: 'windows-latest', suffix: '', flags: '', name: 'Windows (Static)' },
 
-const modes = [
-  [" -DBUILD_SHARED_LIBS=OFF", " Static", "-static"],
-  [" -DBUILD_SHARED_LIBS=ON", " Dylib", ""],
-];
-
 const vulkan = [
   ["", "", ""],
-  [" -DGGML_VULKAN=1", " Vulkan", "-vulkan"],
+  // Vulkan is actually unsupported on GH Actions
+  // [" -DGGML_VULKAN=1", " Vulkan", "-vulkan"],
 ];
 
 const winArch = [
-  { runner: "windows-latest", os: "windows-x64", name: "Windows X64" },
-  { runner: "windows-11-arm", os: "windows-arm64", name: "Windows Arm64" },
+  {
+    runner: "windows-latest",
+    os: "windows-x64",
+    name: "Windows X64",
+    flags: "",
+  },
+  {
+    runner: "windows-11-arm",
+    os: "windows-arm64",
+    name: "Windows Arm64",
+    flags: "-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++",
+  },
 ];
 
 const linuxArch = [
@@ -22,100 +28,87 @@ const linuxArch = [
   { runner: "ubuntu-22.04-arm", os: "linux-arm64", name: "Linux Arm64" },
 ];
 
-const winMatrix = generateAllCombinations(winArch, modes, vulkan)
-  .map((data) => {
-    const [prelude, ...combos] = data;
+const winMatrix = generateAllCombinations(winArch, vulkan).map((data) => {
+  const [prelude, ...combos] = data;
 
-    const out = {
-      ...prelude,
-      suffix: "",
-      flags: "",
-      vulkan: false,
-    };
+  const out = {
+    ...prelude,
+    suffix: "",
+    vulkan: false,
+  };
 
-    combos.forEach((combo) => {
-      const [flag, name, suffix] = combo;
+  combos.forEach((combo) => {
+    const [flag, name, suffix] = combo;
 
-      if (name.length != 0) {
-        out.name += name;
+    if (name.length != 0) {
+      out.name += name;
+    }
+
+    if (flag.length != 0) {
+      out.flags += flag;
+    }
+
+    if (suffix.length != 0) {
+      out.suffix += suffix;
+
+      if (suffix == "-vulkan") {
+        out.vulkan = true;
       }
-
-      if (flag.length != 0) {
-        out.flags += flag;
-      }
-
-      if (suffix.length != 0) {
-        out.suffix += suffix;
-
-        if (suffix == "-vulkan") {
-          out.vulkan = true;
-        }
-      }
-    });
-
-    return out;
-  })
-  .filter((data) => {
-    return data.suffix.startsWith("-static")
-      ? data.suffix == "-static"
-      : data.suffix != "";
+    }
   });
 
-const linuxMatrix = generateAllCombinations(linuxArch, modes, vulkan)
-  .map((data) => {
-    const [prelude, ...combos] = data;
+  return out;
+});
 
-    const out = {
-      ...prelude,
-      vulkan: false,
-      suffix: "",
-      flags: "",
-    };
+const linuxMatrix = generateAllCombinations(linuxArch, vulkan).map((data) => {
+  const [prelude, ...combos] = data;
 
-    combos.forEach((combo) => {
-      const [flag, name, suffix] = combo;
+  const out = {
+    ...prelude,
+    vulkan: false,
+    suffix: "",
+    flags: "",
+  };
 
-      if (name.length != 0) {
-        out.name += name;
+  combos.forEach((combo) => {
+    const [flag, name, suffix] = combo;
+
+    if (name.length != 0) {
+      out.name += name;
+    }
+
+    if (flag.length != 0) {
+      out.flags += flag;
+    }
+
+    if (suffix.length != 0) {
+      out.suffix += suffix;
+
+      if (suffix == "-vulkan") {
+        out.vulkan = true;
       }
-
-      if (flag.length != 0) {
-        out.flags += flag;
-      }
-
-      if (suffix.length != 0) {
-        out.suffix += suffix;
-
-        if (suffix == "-vulkan") {
-          out.vulkan = true;
-        }
-      }
-    });
-
-    return out;
-  })
-  .filter((data) => {
-    return data.suffix.startsWith("-static")
-      ? data.suffix == "-static"
-      : data.suffix != "";
+    }
   });
+
+  return out;
+});
 
 const macosMatrix = [
   {
     runner: "macos-latest",
     os: "macos-arm64",
-    name: "Macos Arm64 Static",
+    name: "Macos Arm64",
     vulkan: false,
-    suffix: "-static",
-    flags: " -DBUILD_SHARED_LIBS=OFF",
+    suffix: "",
+    flags: "",
   },
   {
     runner: "macos-15-intel",
     os: "macos-x64",
-    name: "Macos X64 Static",
+    name: "Macos X64",
     vulkan: false,
-    suffix: "-static",
-    flags: " -DBUILD_SHARED_LIBS=OFF",
+    suffix: "",
+    flags: "",
   },
 ];
 
